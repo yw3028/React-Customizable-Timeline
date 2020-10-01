851e660c1720e34082583e39402f6afa7cc9ab4a
Erik Pastor Rios <erikpastorrios1994@gmail.com>
2020-10-01 16:45:00 +0200

    fix(build): fix build for npm
    fix build for npm

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,23 @@
 var path = require('path');
 module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
   entry: './src/index.tsx',
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'index.js',
-    libraryTarget: 'commonjs2',
+  },
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: ['.ts', '.tsx', '.js'],
   },
   module: {
     rules: [
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
       {
         test: /\.tsx?$/,
-        include: path.resolve(__dirname, 'src'),
-        exclude: /(node_modules|demo|build)/,
-        use: 'ts-loader',
+        loader: 'ts-loader',
       },
     ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  externals: {
-    react: 'commonjs react',
-    'styled-components': {
-      commonjs: 'styled-components',
-      commonjs2: 'styled-components',
-      amd: 'styled-components',
-    },
   },
 };
