abe3e16dcaac5c3480a59ab14eae1b563c83e658
Erik Pastor Rios <erikpastorrios1994@gmail.com>
2020-10-02 17:05:22 +0200

    test(timeline.test.js): add beforeEach and afterEach
    Add beforeEach and afterEach hooks to create the DOM element and delete it

--- src/Components/timeline/Timeline.test.js
+++ src/Components/timeline/Timeline.test.js
@@ -1,10 +1,41 @@
+import { cleanup } from '@testing-library/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import renderer from 'react-test-renderer';
 import Timeline from './Timeline';
 
-test('render correctly', () => {
-  const container = document.createElement('div');
+let container;
+
+beforeEach(() => {
+  container = document.createElement('div');
   ReactDOM.render(<Timeline />, container);
+});
+
+afterEach(() => {
+  cleanup();
+});
+
+test('render correctly', () => {
   expect(container).toMatchSnapshot();
 });
+
+test('Renders the title', () => {});
+
+const data = [
+  {
+    title: 'TITLE 1',
+    events: [
+      {
+        title: 'EVENT TITLE',
+        subtitle: 'EVENT SUBTITLE',
+        content: 'CONTENT',
+        location: 'LOCATION',
+        label: '#LABLE1 #LABLE2',
+        img: {
+          url:
+            'https://images.unsplash.com/photo-1600790078201-5490baf711d6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+          alt: 'pic',
+        },
+      },
+    ],
+  },
+];
