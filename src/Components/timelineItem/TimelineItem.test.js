3dc5811eeff1996660f61515ef5cdbf1f54ce625
Erik Pastor Rios <erikpastorrios1994@gmail.com>
2020-10-02 18:06:52 +0200

    test(timelineitem.test.js): add test for TimelineItem.tsx
    Add test for TimelineItem.tsx

--- src/Components/timelineItem/TimelineItem.test.js
+++ src/Components/timelineItem/TimelineItem.test.js
@@ -1,3 +1,10 @@
-test('true', () => {
-  expect(true).toBe(true);
+import React from 'react';
+import { render } from '@testing-library/react';
+import ReactDOM from 'react-dom';
+import TimelineItem from './TimelineItem';
+
+it('matches snapshot', () => {
+  const container = document.createElement('div');
+  ReactDOM.render(<TimelineItem />, container);
+  expect(container).toMatchSnapshot();
 });
