c9775c7cd09aae33283db5d793c39966221c4cde
Erik Pastor Rios <erikpastorrios1994@gmail.com>
2020-10-03 17:05:18 +0200

    test(codesnippet.test.js): add test for codesnippet
    add test for codesnippet

--- demo/src/Components/CodeSnippet/CodeSnippet.tsx
+++ demo/src/Components/CodeSnippet/CodeSnippet.tsx
@@ -3,47 +3,45 @@ import Prism from 'prismjs';
 import './prism.css';
 
 type OptionProps = {
-    colors: Colors;
-    font: string;
-    shapes: Shapes;
-    animation: boolean;
+  colors: Colors;
+  font: string;
+  shapes: Shapes;
+  animation: boolean;
 };
 
 type Colors = {
-    primaryColor: string;
-    secondaryColor: string;
+  primaryColor: string;
+  secondaryColor: string;
 };
 
 type Shapes = {
-    titleShape: string;
-    dotShape: string;
-    lineShape: string;
+  titleShape: string;
+  dotShape: string;
+  lineShape: string;
 };
 
 const CodeSnippet: React.FC<OptionProps> = ({ colors, font, shapes, animation }) => {
-    useEffect(() => {
-        Prism.highlightAll();
-    });
+  useEffect(() => {
+    Prism.highlightAll();
+  });
 
-    return (
-        <pre>
-            <code className="language-javascript">
-                {`
-    <Timeline
-        data={data}
-        primaryDarkColor='${colors.primaryColor}' 
-        primaryLightColor='${colors.secondaryColor}' 
-        titleShape='${shapes.titleShape}' 
-        dotShape='${shapes.dotShape}' 
-        lineStyle='${shapes.lineShape}' 
-        primaryFont='${font}' 
-        animation={${animation}} 
-    />
-               
-`}
-            </code>
-        </pre>
-    );
+  return (
+    <pre>
+      <code className="language-javascript">
+        {`<Timeline
+              data={data}
+              primaryDarkColor='${colors.primaryColor}' 
+              primaryLightColor='${colors.secondaryColor}' 
+              titleShape='${shapes.titleShape}' 
+              dotShape='${shapes.dotShape}' 
+              lineStyle='${shapes.lineShape}' 
+              primaryFont='${font}' 
+              animation={${animation}}
+          />
+        `}
+      </code>
+    </pre>
+  );
 };
 
 export default CodeSnippet;
